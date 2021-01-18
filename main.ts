59d38c373266c15d771a1b1420c8ea32f0a44edc
Morten Rasmussen <mra@abakion.com>
2021-01-18 18:57:46 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showArrow(ArrowNames.North)
+})
+input.onGesture(Gesture.ScreenDown, function () {
+    basic.showArrow(ArrowNames.West)
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    basic.showArrow(ArrowNames.East)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showArrow(ArrowNames.South)
+})
